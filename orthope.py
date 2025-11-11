1fac2ebddf49ffb71f85367dff4eca8b4a0c46f6
Jack <Taylor@psych.uni-frankfurt.de>
2025-11-11 21:57:29 +0100

    add ot measures

--- orthope.py
+++ orthope.py
@@ -1,24 +1,28 @@
-import csv
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import os
 import re
 import pandas as pd
 import scipy
-import seaborn as sns
 import string
 from pathlib import Path
 import collections
+from itertools import groupby
 from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
 import warnings
 
+import otfuns
+
 #noises = [1E-10, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0]
-noises  = [0.1, 0.2, 0.5, 0.8, 1.0, 1.5, 2.0]
+noises  = [0.0, 0.1, 0.2, 0.5, 0.8, 1.0, 1.5, 2.0]
 min_freq_percs = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]  # 100%, top 90% frequent, top 80% frequent, etc.
 
 special  = 'àâäæçéèêëîïôœùûüÿÀÂÄÆÇÉÈÊËÎÏÔŒÙÛÜŸëïöüĳËÏÖÜĲäöüßÄÖÜẞáéíóúñÁÉÍÓÚÑ'  # special characters to include as alphabetic (in addition to string.ascii_letters)
+
+# self = OrthopeEstimator('german', 'courier', 0.0, ['Tisch', 'Lampe'], data_label='test')
 
 class OrthopeEstimator():
 
@@ -53,7 +57,8 @@ class OrthopeEstimator():
 		self.savepath = Path('models')
 
 		data_label = '' if data_label is None else f'{data_label}_'
-		self.opespath = self.savepath / f'{data_label}{language}_{font}_noise-{noise}_letters-{n_letters[0]}-{n_letters[1]}_freqperc-{freq_perc[0]}-{freq_perc[1]}_freqweight-{freq_weight}_opes.csv'.replace('.','p')
+		opespath_prefix = f'{data_label}{language}_{font}_noise-{noise}_letters-{n_letters[0]}-{n_letters[1]}_freqperc-{freq_perc[0]}-{freq_perc[1]}_freqweight-{freq_weight}_opes'.replace('.','p')
+		self.opespath = self.savepath / f'{opespath_prefix}.csv'
 
 		if not os.path.exists(self.savepath): os.makedirs(self.savepath)
 		if not os.path.exists(self.datapath): os.makedirs(self.datapath)
@@ -63,6 +68,8 @@ class OrthopeEstimator():
 		if estimates is None: 
 			estimates = ['n_pixels_l1', 'n_pixels_l2', 
 						 'pred_err_l1', 'pred_err_l2', 'pw_pred_err', 
+						 'pw_err_wd',
+						#  'pw_err_gwd',
 						 'mahalanobis', 'kalmanw_pred_err']
 		
 		n_obs   = 100 if self.noise > 0 else 1
@@ -91,7 +98,7 @@ class OrthopeEstimator():
 		}
 
 		# Reading corpus
-		datafile = self.corppath / f'{corpora[self.language]['file']}'
+		datafile = self.corppath / f'{corpora[self.language]["file"]}'
 
 		df = pd.read_csv(datafile, sep='\t', encoding='utf-8',
 				   dtype={'word': str, 'raw_freq': int, 'fpmw': float})
@@ -117,7 +124,7 @@ class OrthopeEstimator():
 		df = df.loc[(df.fpmw>=fpmw_filter[0]) & (df.fpmw<=fpmw_filter[1])]
 
 		# Computing corpus at pixel space assuming identical obs_noise
-		dd = np.array([self.__render_text__(word) for word in df['word']])
+		dd = np.array([self.__render_text__(word, noise=self.noise) for word in df['word']])
 
 		return dd, df['fpmw']
 
@@ -162,8 +169,8 @@ class OrthopeEstimator():
 
 		return None
 	
-	def __plot_2d_from_flat__(self, x_1d, cmap='Greys', **kwargs):
-		x_2d = x_1d.reshape((self.canvas_dims[1], self.canvas_dims[0]))
+	def __plot_2d_from_flat__(self, x_1d, cmap='binary', **kwargs):
+		x_2d = x_1d.reshape(self.array_dims)
 		fig, ax = plt.subplots()
 		im = ax.imshow(x_2d, interpolation='none', cmap=cmap, **kwargs)
 		divider = make_axes_locatable(ax)
@@ -171,7 +178,7 @@ class OrthopeEstimator():
 		fig.colorbar(im, cax=cax, orientation='vertical')
 		return fig, ax
 	
-	def __plot_2dstat__(self, stat, log_trans=False, cmap='Greys'):
+	def __plot_2dstat__(self, stat, log_trans=False, cmap='binary'):
 		if not hasattr(self, 'corpus_stats') or stat not in self.corpus_stats:
 			print(f'{stat} not (yet) estimated via estimate_corpus_stats')
 		else:
@@ -190,7 +197,7 @@ class OrthopeEstimator():
 		if not hasattr(self, 'corpus_stats') or stat not in self.corpus_stats:
 			print(f'{stat} not (yet) estimated via estimate_corpus_stats')
 		else:
-			stat_4d = self.corpus_stats[stat].reshape((self.canvas_dims[1], self.canvas_dims[0], self.canvas_dims[1], self.canvas_dims[0]))
+			stat_4d = self.corpus_stats[stat].reshape((self.array_dims[0], self.array_dims[1], self.array_dims[0], self.array_dims[1]))
 
 			stat_lab = f'log {stat}' if log_trans else stat
 
@@ -235,7 +242,7 @@ class OrthopeEstimator():
 	def plot_stat(self, stat):
 		match stat:
 			case 'mu':
-				self.__plot_2dstat__(stat, log_trans=False, cmap='Greys')
+				self.__plot_2dstat__(stat, log_trans=False, cmap='binary')
 			case 'sigma':
 				self.__plot_4dstat__(stat)
 			case 'pi':
@@ -247,9 +254,10 @@ class OrthopeEstimator():
 
 	def __estimate_ope__(self, word, estimate):
 
-		x = self.__render_text__(word)
+		x = self.__render_text__(word, noise=self.noise)
+		# x_no_noise = self.__render_text__(word, noise=0.0)
 
-		if 'n_pixels_' not in estimate:
+		if 'n_pixels_' not in estimate and '_wd' not in estimate and '_gwd' not in estimate:
 			e = x - self.corpus_stats['mu']
 
 		match estimate:
@@ -264,6 +272,20 @@ class OrthopeEstimator():
 				ope = np.linalg.norm(e)
 			case 'pw_pred_err':
 				ope = np.linalg.norm(e * self.corpus_stats['pi_id'])
+			case 'pw_err_wd':
+				if self.font == 'word' or self.noise!=0.0:
+					ope = np.nan
+				else:
+					ope = otfuns.get_w(
+						s = x.reshape(self.array_dims),
+						t = self.corpus_stats['mu'].reshape(self.array_dims))
+			case 'pw_err_gwd':
+				if self.font == 'word' or self.noise!=0.0:
+					ope = np.nan
+				else:
+					ope = otfuns.get_gw(
+						s = x.reshape(self.array_dims),
+						t = self.corpus_stats['mu'].reshape(self.array_dims))
 			case 'mahalanobis':
 				if np.size(self.corpus_stats['pi'])==1 and np.isnan(self.corpus_stats['pi']):
 					ope = np.nan
@@ -277,11 +299,15 @@ class OrthopeEstimator():
 
 		return ope
 
-	def __render_text__(self, text, show=False):
+	def __render_text__(self, text, noise=0.0, show=False):
 
 		# Settings
 		font_size   = 34
-		self.canvas_dims = (int(round(22*self.n_letters[1])), 36)
+		canvas_dims = (int(round(22*self.n_letters[1])), 36)
+
+		if not hasattr(self, 'array_dims'):
+			self.array_dims = (canvas_dims[1], canvas_dims[0])
+		
 		font_dict   = {'courier'  : self.fontpath / 'couriernew.ttf',
 					   'courieri' : self.fontpath / 'couriernewi.ttf',
 					   'cambria'  : self.fontpath / 'cambria.ttf',
@@ -289,19 +315,76 @@ class OrthopeEstimator():
 					   'cambriai' : self.fontpath / 'cambriai.ttf'}
 
 		# Rendering text with pillow
-		render   = Image.new('L', self.canvas_dims, color=0)
+		render   = Image.new('L', canvas_dims, color=0)
 		draw     = ImageDraw.Draw(render)
 		font     = ImageFont.truetype(font_dict[self.font], font_size)
-		text_pos = ((self.canvas_dims[0] - font.getlength(text))/2, -7)
+		text_pos = ((canvas_dims[0] - font.getlength(text))/2, -7)
 		draw.text(text_pos, text, fill=255, font=font)
 		if show: render.show();
 
 		# Applying additive Gaussian noise
 		render_array = np.array(render) / 255 # Normalise to r \in [0, 1]
-		noise_array  = self.noise * np.random.randn(*render_array.shape)
+		noise_array  = noise * np.random.randn(*render_array.shape)
 		text_array   = (render_array + noise_array).flatten()
 
 		return text_array
+	
+	def __get_letter_space_locs__(self, x_2d):
+		# Detects the locations of spaces between letters, assuming that there are no breaks along the x axis within glyphs of a width greater than 12 pixels.
+		max_xaxis = x_2d.max(axis=0)
+
+		# dummy code the start and end to the max, so they can be used to detect start and end of word (because of the peak-finding algorithm used)
+		max_xaxis[0] = max_xaxis.max()
+		max_xaxis[len(max_xaxis)-1] = max_xaxis.max()
+
+		# use peak-finding algorithm to get the spaces' locations
+		space_centres, _ = sp.signal.find_peaks(-max_xaxis, distance=12)  # minimum distance is assumed to be less than the expected width of a character
+
+		space_locs = space_centres[1:-1]  # now ignore the zeroes at the starts and ends of the words
+
+		assert len(space_locs) >= self.n_letters[0]-1, f'Detected {len(space_locs)} spaces in a word image, but expected the min to be {self.n_letters[0]-1}'
+		assert len(space_locs) <= self.n_letters[1]-1, f'Detected {len(space_locs)} spaces in a word image, but expected the max to be {self.n_letters[1]-1}'
+
+		return space_locs
+	
+	def __split_word_img_letters__(self, x_2d, space_locs=None):
+		# Input x_2d should be a 2d array of the word, with no noise.
+		# Returns an image for each detected letter in the word, with zeroes where the other characters were (can preserve the dimensions of the input in each output)
+		if space_locs is None:
+			space_locs = self.__get_letter_space_locs__(x_2d=x_2d)
+		space_locs = np.insert(space_locs, 0, 0.0)
+
+		x_2d_spl = []
+		for i in range(len(space_locs)):
+			x_2d_i = x_2d.copy()
+
+			if i > 0:
+				x_2d_i[:, :space_locs[i]] = 0.0
+
+			if i < len(space_locs)-1:
+				x_2d_i[:, space_locs[i+1]:] = 0.0
+				
+			x_2d_spl.append( x_2d_i )
+
+		return x_2d_spl
+	
+	def __crop_to_content__(self, x, y_2d=None):
+		if len(x.shape)==2:
+			# find non-zero elements
+			nonzero_idx = np.where(x!=0.0) if y_2d is None else np.where(y_2d!=0.0)
+			# create list of slices by which x should be indexed
+			crop_idx = [slice(np.min(d), np.max(d)+1) for d in nonzero_idx]
+			x_crop = x[tuple(crop_idx)]
+		elif len(x.shape)==3:
+			# find non-zero elements (averaging across first axis of x)
+			nonzero_idx = np.where(x.mean(axis=0)!=0.0) if y_2d is None else np.where(y_2d!=0.0)
+			# create list of slices by which x should be indexed
+			crop_idx = [slice(np.min(d), np.max(d)+1) for d in nonzero_idx]
+			crop_idx.insert(0, slice(None))
+			x_crop = x[tuple(crop_idx)]
+		else:
+			raise ValueError('x must be 2d or 3d')
+		return(x_crop)
 
 	def load_opes(self, input_words=None):
 
@@ -329,7 +412,7 @@ class LetterOrthopeEstimator(OrthopeEstimator):
 	def __init__(self, language, noise, input_words, n_letters=(5, 5), freq_perc=(0, 100), freq_weight=True, data_label=None):
 		super().__init__(language, font='word', noise=noise, input_words=input_words, n_letters=n_letters, freq_perc=freq_perc, freq_weight=freq_weight, data_label=data_label)
 
-	def __render_text__(self, text, show=False):
+	def __render_text__(self, text, noise=0.0, show=False):
 
 		# Settings
 		alphabet = self.alphabet
@@ -340,11 +423,52 @@ class LetterOrthopeEstimator(OrthopeEstimator):
 		for cix, c in enumerate(text):
 			render_array[cix, alphabet.index(c)] = 1
 		
-		noise_array = self.noise * np.random.randn(*render_array.shape)
+		noise_array = noise * np.random.randn(*render_array.shape)
 		text_array  = (render_array + noise_array).flatten()
 
 		return text_array
 
+
+class OptimalTransportOrthopeEstimator(OrthopeEstimator):
+
+	def __init__(self, language, font, noise, input_words, n_letters=(5, 5), freq_perc=(0, 100), freq_weight=True, data_label=None):
+		super().__init__(language, font=font, noise=noise, input_words=input_words, n_letters=n_letters, freq_perc=freq_perc, freq_weight=freq_weight, data_label=data_label)
+	
+	def estimate_corpus_stats(self, weight_by_freq=True):
+		
+		print('Rendering corpus...')
+		dd, weights = self.__render_corpora__()
+
+		if not weight_by_freq:
+			weights = np.ones(weights.shape)
+
+		# Estimating stats
+		print('Estimating word-level barycentre...')
+		dd_3d = dd.reshape([-1, self.array_dims[0], self.array_dims[1]])  # reshape to 3d array of word * x * y
+		bc = otfuns.get_w_barycentre(dd_3d, debias=False, weights=weights, reg=0.0005, numItermax=int(1e7))
+
+		del dd_3d
+
+		print('Estimating within-letter barycentres...')
+		dd_spl = [self.__split_word_img_letters__(dd_i.reshape(self.array_dims)) for dd_i in dd]
+
+		del dd
+
+		lett_slot_3d = []
+		for i in range(self.n_letters[1]):
+			lett_slot_3d_i = []
+			for dd_spl_j in dd_spl:
+				if len(dd_spl_j) >= (i + 1):
+					lett_slot_3d_i.append(dd_spl_j[i])
+			lett_slot_3d.append(np.array(lett_slot_3d_i))
+
+		del dd_spl
+
+		bc_lett_slot = [otfuns.get_w_barycentre(lett_slot_3d_i, debias=False, weights=weights, reg=0.0005, numItermax=int(1e7)) for lett_slot_3d_i in lett_slot_3d]
+
+		self.corpus_stats = {'bc': bc, 'bc_lett_slot': bc_lett_slot}
+
+		return None
 
 
 def run_all_oPEs(language, font, input_words, n_letters=(5, 5), data_label=None):
