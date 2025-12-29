f170c4916b57ca12c7a6a355ed62091b1b36e6d9
Jack <Taylor@psych.uni-frankfurt.de>
2025-12-29 09:34:18 +0100

    update pipelines

--- pipelines.py
+++ pipelines.py
@@ -8,13 +8,15 @@ import time
 def compute_all_models(language, input_words, fonts=None, n_letters=(5, 5), data_label=None):
 
 	if (fonts is None) or (fonts == 'word'):
-		fonts = ['word']
+		orthope.run_all_oPEs(language=language, font='word', input_words=input_words, n_letters=n_letters, data_label=data_label)
+	else:
+		for font in fonts:
+			# for prior_font in fonts:
+			# 	orthope.run_all_oPEs(language=language, font=font, prior_font=prior_font, input_words=input_words, n_letters=n_letters, data_label=data_label)
+			
+			orthope.run_all_oPEs(language=language, font=font, prior_font=font, input_words=input_words, n_letters=n_letters, data_label=data_label)
 
-	for font in fonts:
-		orthope.run_all_oPEs(language=language, font=font, input_words=input_words, n_letters=n_letters, data_label=data_label)
-
-
-fonts = ['courier', 'courieri', 'cambria', 'verdana', 'cambriai']
+fonts = ['courier', 'courieri', 'cambria', 'verdana', 'cambriai', 'comic']
 language = 'german'
 
 # calculate for all German stimuli presented in Gagl et al.
@@ -22,7 +24,7 @@ dh = datahandlers.Gagl2020DataHandler(language=language)
 input_words = dh.get_unique_words()
 nletters_lims = dh.get_nletter_lims()
 
-compute_all_models(language, input_words=input_words, fonts='word', n_letters=nletters_lims, data_label='gagl2020')
 compute_all_models(language, input_words=input_words, fonts=fonts, n_letters=nletters_lims, data_label='gagl2020')
+compute_all_models(language, input_words=input_words, fonts='word', n_letters=nletters_lims, data_label='gagl2020')
 
 print('done!')
