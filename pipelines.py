4604cc228613d341aee71d6ec99c94bfe3f7b80b
Jack <Taylor@psych.uni-frankfurt.de>
2026-03-11 14:05:50 +0100

    typo

--- pipelines.py
+++ pipelines.py
@@ -2,7 +2,7 @@ import orthope
 import datahandlers
 import sys
 
-if len(sys.argv)>0:
+if len(sys.argv)>1:
     subset = [int(sys.argv[1]), int(sys.argv[2])]
     n_jobs = int(sys.argv[3])
 else:
@@ -13,7 +13,6 @@ language = 'german'
 
 # calculate for all German stimuli presented in Gagl et al.
 dh = datahandlers.Gagl2020DataHandler(language=language)
-input_words = dh.get_unique_words()
 nletters_lims = dh.get_nletter_lims()
 
 orthope.run_all_oPEs(language=language, input_words=input_words, n_letters=nletters_lims, data_label='gagl2020', n_jobs=n_jobs, save_at_each=True, joblib_backend='loky', subset=subset)
